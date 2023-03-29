6c020f80a2e3645a65234e232139bce7d4f92b35
danmark-quinones <Danmark.Quinones@au.harveynorman.com>
2023-03-29 15:53:05 +0800

    updates desing for collectio caart and products

--- assets/application.js
+++ assets/application.js
@@ -11,3 +11,42 @@ closeMenuButton.addEventListener("click" , function(){
     menuContainer.classList.add("hide")
     menuContainer.classList.remove("show")
 })
+
+let inventoryHash = document.querySelectorAll('[inventorymanagment]'); 
+
+Array.from(inventoryHash).forEach( 
+    (Selectedvariant)=>{ 
+
+      if(Selectedvariant.dataset.id == variant.id){ 
+
+        if(Selectedvariant.dataset.inventory > 0) 
+        { 
+          $("#variant-inventory").html("We have "+ Selectedvariant.dataset.inventory +" in stock"); 
+
+        } 
+        else{ 
+          $("#variant-inventory").html("Sold out!"); 
+
+        } 
+      } 
+    } 
+  );
+  
+  function instantBuy(){
+    var $ = jQuery;
+    var formParams = $('form.cart').serialize();
+    $.ajax({
+     url: "/cart/add",
+     type: "post",
+     data: formParams,
+     success: function(){
+      window.location.href = "/checkout";
+     },
+     error: function(){
+     }
+    })
+   }
+
+   function addToCart(){
+    $('form.cart').submit();
+   }
